99538d2f4a811fbfb1cb6b1655b2b4c262de3dc2
Duong Tuan <tuanda5353@gmail.com>
2019-01-05 15:29:41 +0700

    Show data on Ui part 2

--- app/src/main/java/framgia/com/mynote/data/model/Callback.java
+++
@@ -1,9 +0,0 @@
-package framgia.com.mynote.data.model;
-
-import java.util.List;
-
-public interface Callback {
-    void onGetDataSuccess(List<Note> notes);
-
-    void onGetDataFailed(Exception e);
-}
--- app/src/main/java/framgia/com/mynote/data/source/NoteDataSource.java
+++
@@ -1,9 +0,0 @@
-package framgia.com.mynote.data.source;
-
-import framgia.com.mynote.data.model.Callback;
-
-public interface NoteDataSource {
-    interface Local {
-        void getNotes(Callback callback);
-    }
-}
