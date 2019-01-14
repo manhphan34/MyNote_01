91661229d6852356385a7606d6fa0c9090ed55d3
Duong Tuan <tuanda5353@gmail.com>
2019-01-14 10:16:21 +0700

    Creat UI widget

---
+++ app/src/main/java/framgia/com/mynote/widget/NoteAppWidget.java
@@ -0,0 +1,38 @@
+package framgia.com.mynote.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import framgia.com.mynote.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NoteAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.note_app_widget);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+
+    }
+}
+
